cb947f6d69ca942cbe64516d5e668cc38babbddc
ChrisAggelUop <ece21094@go.uop.gr>
2025-12-02 21:54:15 +0200

    mux2_6.vhd

--- mux2_6.vhd
+++ mux2_6.vhd
@@ -3,15 +3,16 @@ use ieee.std_logic_1164.all;
 
 entity mux2_6 is
     port (
-        in0  : in  std_logic_vector(5 downto 0); -- Είσοδος 0
-        in1  : in  std_logic_vector(5 downto 0); -- Είσοδος 1
-        sel  : in  std_logic;                    -- Σήμα επιλογής (1 bit)
-        dout : out std_logic_vector(5 downto 0)  -- Έξοδος (6 bits)
+        in0  : in  std_logic_vector(5 downto 0); 
+        in1  : in  std_logic_vector(5 downto 0); 
+        sel  : in  std_logic;                   --s0, s1--
+        dout : out std_logic_vector(5 downto 0)  
     );
 end mux2_6;
 
 architecture dataflow of mux2_6 is
 begin
-    -- Όταν sel='0' δίνει έξοδο το in0, αλλιώς το in1
+
     dout <= in0 when sel = '0' else in1;
+
 end dataflow;
